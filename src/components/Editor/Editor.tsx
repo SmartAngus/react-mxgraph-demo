bc5b34c7e668b23ee15b42c94778418379113e01
Jezdimir Loncar <jezdimir.loncar@gmail.com>
2020-08-16 19:36:06 +0200

    Grid and theming

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,22 +1,38 @@
 import React, { useEffect, useContext } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme  } from '@material-ui/core/styles';
 import './Editor.css';
 import { Editor as EditorJS, EditorContext } from '../../editor';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    content: {
+    wrapper: {
         padding: theme.spacing(3),
     },
+    content: {
+        zIndex: 0
+    },
 }));
+
+const calculateContentHeight = (tbHeight:number, paddingOffset:number):string => {
+
+    if(typeof window === "undefined")
+        return "100%";
+
+    const height:number = (window.innerHeight - tbHeight - paddingOffset) * 0.9;
+
+    return `${height}px`;
+}
 
 export default function Editor() {
 
     const classes = useStyles()
     const editorCtx = useContext(EditorContext);
-
+    const theme = useTheme();
 
     useEffect(() => {
         EditorJS.init()
+        EditorJS.addGrid()
         EditorJS.addDemoWidgets()
         if(EditorJS.initialized && editorCtx.setInitialized)
             editorCtx.setInitialized(true)
@@ -25,7 +41,21 @@ export default function Editor() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    // Calculate toolbar height
+    let tbHeight = 56;
+    const xsTb = useMediaQuery(`${theme.breakpoints.up('xs')} and (orientation: landscape)`);
+    if(xsTb)
+        tbHeight = 48;
+    const smTb = useMediaQuery(`${theme.breakpoints.up('sm')}`);
+    if(smTb)
+        tbHeight = 64;
+
+
     return (
-        <div className={"graph-content " + classes.content} key="content" />
+        <div className={"graph-wrapper " + classes.wrapper}>
+            <Paper elevation={3}
+                   className={"graph-content " + classes.content}
+                   style={{"minHeight": calculateContentHeight(tbHeight, theme.spacing(3))}} />
+        </div>
     )
 };
--- src/editor/editor.tsx
+++ src/editor/editor.tsx
@@ -4,12 +4,12 @@ const mxnSpace = require("mxgraph")()
 class Editor {
 
   initialized: boolean;
-  containerSelector: string|Element;
+  containerSelector: string | Element;
   mxnSpace: any;
-  container?: Element|null;
+  container?: Element | null;
   graph: any; // No TS support for mxgraph
 
-  constructor(containerSelector: string|Element) {
+  constructor(containerSelector: string | Element) {
     this.initialized = false;
     this.containerSelector = containerSelector;
     this.mxnSpace = mxnSpace;
@@ -26,6 +26,9 @@ class Editor {
     // Start editor configuration
     this.graph = new mxnSpace.mxGraph(this.container);
     this.graph.gridSize = 30;
+
+    // Resize container
+    // this.graph.setResizeContainer(true);
 
     // Configure Zoom
     this.graph.keepSelectionVisibleOnZoom = true;
@@ -50,6 +53,134 @@ class Editor {
     } finally {
       this.graph.getModel().endUpdate();
     }
+  }
+
+  repaintGrid(canvas: HTMLCanvasElement, ctx?: CanvasRenderingContext2D | null): void {
+    var s = 0;
+    var gs = 0;
+    var tr = new mxnSpace.mxPoint();
+    var w = 0;
+    var h = 0;
+    if (!ctx) {
+      throw new Error("GRID REDRAW  - Unable to access context")
+    }
+    var bounds = this.graph.getGraphBounds();
+    var width = Math.max(bounds.x + bounds.width, this.graph.container.clientWidth);
+    var height = Math.max(bounds.y + bounds.height, this.graph.container.clientHeight);
+    var sizeChanged = width !== w || height !== h;
+
+    if (this.graph.view.scale !== s || this.graph.view.translate.x !== tr.x || this.graph.view.translate.y !== tr.y ||
+      gs !== this.graph.gridSize || sizeChanged) {
+      tr = this.graph.view.translate.clone();
+      s = this.graph.view.scale;
+      gs = this.graph.gridSize;
+      w = width;
+      h = height;
+
+      // Clears the background if required
+      if (!sizeChanged) {
+        ctx.clearRect(0, 0, w, h);
+      }
+      else {
+        canvas.setAttribute('width', String(w));
+        canvas.setAttribute('height', String(h));
+      }
+
+      var tx = tr.x * s;
+      var ty = tr.y * s;
+
+      // Sets the distance of the grid lines in pixels
+      var minStepping = this.graph.gridSize;
+      var stepping = minStepping * s;
+
+      if (stepping < minStepping) {
+        var count = Math.round(Math.ceil(minStepping / stepping) / 2) * 2;
+        stepping = count * stepping;
+      }
+
+      var xs = Math.floor((0 - tx) / stepping) * stepping + tx;
+      var xe = Math.ceil(w / stepping) * stepping;
+      var ys = Math.floor((0 - ty) / stepping) * stepping + ty;
+      var ye = Math.ceil(h / stepping) * stepping;
+
+      xe += Math.ceil(stepping);
+      ye += Math.ceil(stepping);
+
+      var ixs = Math.round(xs);
+      var ixe = Math.round(xe);
+      var iys = Math.round(ys);
+      var iye = Math.round(ye);
+
+      // Draws the actual grid
+      ctx.strokeStyle = '#f6f6f6';
+      ctx.beginPath();
+
+      for (var x = xs; x <= xe; x += stepping) {
+        x = Math.round((x - tx) / stepping) * stepping + tx;
+        var ix = Math.round(x);
+
+        ctx.moveTo(ix + 0.5, iys + 0.5);
+        ctx.lineTo(ix + 0.5, iye + 0.5);
+      }
+
+      for (var y = ys; y <= ye; y += stepping) {
+        y = Math.round((y - ty) / stepping) * stepping + ty;
+        var iy = Math.round(y);
+
+        ctx.moveTo(ixs + 0.5, iy + 0.5);
+        ctx.lineTo(ixe + 0.5, iy + 0.5);
+      }
+
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }
+
+  addGrid(): void {
+
+    try {
+      const canvas = document.createElement('canvas');
+      canvas.style.position = 'absolute';
+      canvas.style.top = '0px';
+      canvas.style.left = '0px';
+      canvas.style.zIndex = '-1';
+      this.graph.container.prepend(canvas);
+
+      const ctx = canvas.getContext('2d');
+
+      // Modify event filtering to accept canvas as container
+      var mxGraphViewIsContainerEvent = mxnSpace.mxGraphView.prototype.isContainerEvent;
+      mxnSpace.mxGraphView.prototype.isContainerEvent = function (evt: any) {
+        return mxGraphViewIsContainerEvent.apply(this, arguments) ||
+          mxnSpace.mxEvent.getSource(evt) === canvas;
+      };
+
+      var mxGraphViewValidateBackground = mxnSpace.mxGraphView.prototype.validateBackground;
+      const editorInstance = this;
+      mxnSpace.mxGraphView.prototype.validateBackground = function () {
+        mxGraphViewValidateBackground.apply(this, arguments);
+        editorInstance.repaintGrid(canvas, ctx);
+      };
+
+
+    }
+    catch (e) {
+      mxnSpace.mxLog.show();
+      mxnSpace.mxLog.debug('Using background image');
+
+      // mxnSp.container.style.backgroundImage = 'url(\'editors/images/grid.gif\')';
+    }
+
+
+  }
+
+  removeGrid(): void {
+    // @TODO Implement
+  }
+
+
+  fit(): void {
+    this.graph.fit()
   }
 
 }
